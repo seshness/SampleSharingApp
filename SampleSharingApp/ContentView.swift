cc5330fc0539f48e19e85e9c8eefb7e805494786
Seshadri Mahalingam <seshadri@berkeley.edu>
2024-03-29 19:18:38 -0700

    Initial Commit

---
+++ SampleSharingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleSharingApp
+//
+//  Created by Seshadri Mahalingam on 2024-03-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleSharingApp/SampleSharingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSharingAppApp.swift
+//  SampleSharingApp
+//
+//  Created by Seshadri Mahalingam on 2024-03-29.
+//
+
+import SwiftUI
+
+@main
+struct SampleSharingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
